ca796f6bbc9049e0b22d63cfcc6fa4817210b1da
wutangfinancial <35402234+wutangfinancial@users.noreply.github.com>
2021-03-10 22:21:47 -0500

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-apt_packages=(git net-tools speedtest-cli gcc cpp make iptables nginx certbot)
+apt_packages=(git net-tools speedtest-cli gcc cpp make iptables nginx certbot mutt)
 
 apt -y update
 apt -y upgrade
 for p in ${apt_packages[@]}; do
   apt -y install $p
 done
+
+curl https://www.rainloop.net/repository/webmail/rainloop-community-latest.zip
